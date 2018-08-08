ad61713cc916734060e955bf3cb57e97c33aced7
Luan Fagundes <luan@Luan.local>
2018-08-08 16:05:43 +1000

    bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,19 @@
+'''
+Bubble Sort
+'''
+
+l = [30, 50, 10, 35, 70, 45, 80, 100, 22]
+size_l = len(l)
+
+# bubble sort
+for i in range(size_l):
+    change = False # optimization to not check twice 
+
+    for j in range(1, size_l - i):
+        if l[j] < l[j - 1]:
+            l[j], l[j - 1] = l[j - 1], l[j]
+            change = True
+    if not change:
+        break
+
+print(l)
